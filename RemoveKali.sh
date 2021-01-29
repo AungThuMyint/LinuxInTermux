6062e9ce68c8892b8b73f4cef2839203a3da0972
AungThuMyint <aungthumyint.com@gmail.com>
2021-01-30 02:45:56 +0630

    Update

---
+++ RemoveKali.sh
@@ -0,0 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+echo -e "\n\e[91mUninstalling Kali@LocalHost ..."
+echo -e "\n\e[92mBye Bye ...\n"
+
+rm -rf kali-cli
+rm -rf kali-cli.sh
+rm -rf kali-tmp
