b6f8ffb8fa72b32f53335973271121aa70892a25
Aung Thu Myint <63327282+AungThuMyint@users.noreply.github.com>
2021-01-30 01:55:39 +0630

    Update Kali.sh

--- Kali.sh
+++ Kali.sh
@@ -80,7 +80,9 @@ chmod +x $folder/root/Sudo.sh
 termux-fix-shebang $bin
 chmod +x $bin
 rm $tarball
-rm kali-tmp
+rm -rf kali-tmp
 clear
 echo -e "\e[93mYou Can Launch Kali With The ./${bin} Script (Or) bash ${bin} Script."
 echo -e "\e[92mThank You."
+echo -e "\e[92mWelcome | Kali "
+bash $bin
